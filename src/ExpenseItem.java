f08e3e608bca2051e8950190d32be6b4eb9f6a97
machinsk <machinsk@gmail.com>
2015-01-27 11:53:33 -0700

    Added project

---
+++ src/ExpenseItem.java
@@ -0,0 +1,62 @@
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Currency;
+
+
+public class ExpenseItem {
+	protected Date date;
+	protected String currencyUnit;
+	protected String category;
+	protected String textDescprition;
+	protected BigDecimal amountSpent;
+	
+	
+	
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
+	
+	public String getCurrencyUnit() {
+		return currencyUnit;
+	}
+	
+	public void setCurrencyUnit(String currencyUnit) {
+		this.currencyUnit = currencyUnit;
+	}
+	
+	
+	
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	
+	
+	public String getTextDescprition() {
+		return textDescprition;
+	}
+	
+	public void setTextDescprition(String textDescprition) {
+		this.textDescprition = textDescprition;
+	}
+	
+	
+	
+	public BigDecimal getAmountSpent() {
+		return amountSpent;
+	}
+	
+	public void setAmountSpent(BigDecimal amountSpent) {
+		this.amountSpent = amountSpent;
+	}
+	
+	
+}
---
+++ src/TravelClaim.java
@@ -0,0 +1,44 @@
+import java.util.Date;
+
+
+
+public class TravelClaim {
+	protected Date startDate;
+	protected Date endDate;
+	protected String name;
+	protected String textDescription;
+	
+	
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	
+	
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	public String getTextDescription() {
+		return textDescription;
+	}
+	public void setTextDescription(String textDescription) {
+		this.textDescription = textDescription;
+	}
+	
+	
+}
